8c07a0f69b32d771fed0511deb7c41ae5dbf420f
SkimmyMilk937 <110652867+SkimmyMilk937@users.noreply.github.com>
2022-11-06 15:11:56 -0600

    Update PythonInPython.py
    apple needs fixing, I think snakeBod is being appened, creating a new label, but label  is not being assigned x/y because snakeXs and snakeYs need an appened value.
    
    To determine the new value needed to be appened for snakeXs and snakeYs,  python needs to track its last pixel position and create a new segment there after movement.

--- PythonInPython.py
+++ PythonInPython.py
@@ -2,12 +2,10 @@ import collections
 from tkinter import *
 from PIL import Image, ImageTk
 import random
-import time
-
+import pygame
 
 class Snake:
     __slots__ = ( "direction", "x", "y")
-
 
     def __init__(self, direction = "right", x=4, y=7):
         self.direction = direction
@@ -46,7 +44,7 @@ def draw_snake():
     for x in range(len(snakeBod)):
         p = (Label(root, image = redBsour))
         snakelabel.append(p)
-        p.grid( row = snakeYs[x], column = snakeXs[x])
+        p.grid(row = snakeYs[x], column = snakeXs[x]) #add a snake y and a snake x after apple is eaten, only bod is being appened rn'
         p.grid()
 
     
@@ -71,7 +69,7 @@ def move_add():
         print(head.x)
     elif head.direction =="up":
         head.y = head.y -1 
-        print(head.y)   #not adding values for some reason
+        print(head.y)   
     elif head.direction == "left":
         head.x = head.x -1 
         print(head.x)
@@ -96,44 +94,53 @@ def dircL():
         head.direction = "left"
         print(head.direction)
 
-def spawnApple():
+def apple_update():
     global apple_label
-    
+
     if apple.is_spawn==False:
        for pixel in pixels:
            if pixel.x == apple.x and pixel.y == apple.y:
                apple_label = Label(root, image = applesour)
                apple_label.grid(row = pixel.y, column = pixel.x, columnspan=1, rowspan=1, pady= 0, padx=0)
-               apple.rollnums()
                pixel.color = "red"
-               apple.is_spawn=True 
+               apple.is_spawn=True     
+               print(" apple_update")
                break
-
-def snakeEat():                         #makesure this runs somewhere in end
-    if snakeXs[0] == apple.x and snakeYs[0] == apple.y:
+               
+    if (snakeXs[0] == apple.x and snakeYs[0] == apple.y):
         snakeBod.append(Snake()) ##d
         apple.is_spawn == False
-        apple_label.after(1, apple_label.destory)
+        apple_label.after(1, apple_label.destroy)
+        print(" apple_update pos")               
+        apple.rollnums()
 
-gameon=True
-apple = Apple()
-head = Snake()
+    
 root = Tk()
 root.resizable(0,0)
 root.title("PythonInPython")
 root.iconbitmap('python32px.ico')
+
 greenBsour = ImageTk.PhotoImage(Image.open("GreenBlock.jpg"))
 redBsour = ImageTk.PhotoImage(Image.open("RedBlock.png"))
 applesour = ImageTk.PhotoImage(Image.open("apple.png"))
+
 root.bind("<w>", lambda event: dircU())
 root.bind("<a>", lambda event: dircL())
-root.bind("<s>", lambda event: dircD()) #asynch this
+root.bind("<s>", lambda event: dircD()) 
 root.bind("<d>", lambda event: dircR())
+
+gameon=True
+apple = Apple()
+head = Snake()
+
 snakeXs = []
 snakeYs = []
 pixels = []
 snakeBod = [] #holds snake object
 snakelabel = [] #holds snake labels
+
+start_clock = pygame.time.Clock()
+if_clock = 0
 
 for r in range(15):
     for c in range(17):
@@ -142,20 +149,16 @@ for pixel in pixels:
     Label(root, image = greenBsour).grid(row = pixel.y, column = pixel.x, columnspan=1, rowspan=1, pady= 0, padx=0)
     pixel.color = "green"
 
-d = 4
 for c in range(3):
     snakeBod.append(Snake())
-    snakeXs.append(d)
+    snakeXs.append(4-c)
     snakeYs.append(7)
-    d+=-1
+
 
 while gameon == True:
-    time.sleep(.4)
     snakeXs, snakeYs = move(snakeXs, snakeYs)
     print(snakeXs, snakeYs, len(snakeBod), len(snakelabel))  #fix itteration with direction adn apple spawning/destroyiong
-    snakeEat()
-    spawnApple()
-    snakeXs, snakeYs = move(snakeXs, snakeYs)
+    apple_update()
     root.update()
-
+    start_clock.tick(3)
 
